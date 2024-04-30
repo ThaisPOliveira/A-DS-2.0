351268b05e7555770da58ebadfc0a428465ca170
EDUARDONEVES120206 <159830002+EDUARDONEVES120206@users.noreply.github.com>
2024-04-30 18:52:54 -0300

    Update CRUD.py

--- CRUD/CRUD.py
+++ CRUD/CRUD.py
@@ -1,21 +1,16 @@
-#UMC ADS - 1°D
-#Nomes: 
-#       Eduardo Vicente Ferreira das Neves: 11241104345
-#       Erick Fernando Martins Santos: 11241504537
-#       Rafael da Silva Castro: 11241104987
-#       Thais Perreira de Oliveira: 11241103553
-
-#Pré Projeto Crud em Python
-import os, sqlite3,pwinput
+import os
+import sqlite3
+import pwinput
 from datetime import datetime
-estrato=[]
+
+estrato = []
 os.system('cls')
+
 # Conexão com o banco de dados SQLite
 conn = sqlite3.connect('banco.db')
 cursor = conn.cursor()
 
 # Criando a tabela de usuários se não existir
-
 cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
                     username TEXT PRIMARY KEY,
                     password TEXT,
@@ -23,26 +18,31 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
                 )''')
 
 conn.commit()
+
+
 def extrato():
-    arquivo_estrato=open("CRUD\estrato.txt","w")
-    arquivo_estrato.write(f"{"EXTRATO":^50}\n")
+    arquivo_estrato = open("CRUD\estrato.txt", "w")
+    arquivo_estrato.write(f"{'EXTRATO':^50}\n")
     arquivo_estrato.write(f"{username:^50}\n")
-    for i,(usuario,hora,relatorio) in enumerate(estrato):
+    for i, (usuario, hora, relatorio) in enumerate(estrato):
         arquivo_estrato.write(f"{relatorio} {hora} \n")
     conn.close()
+
+
 def Cadastro_usuario(username, password):
     cursor.execute("SELECT * FROM usuarios WHERE username = ?", (username,))
     user = cursor.fetchone()
     if user:
-        return "Nome de usuário já existe. Por favor, escolha outro."
-    elif username==password:
-        print("Usuario e senhas iguais, tente novamente")
-    elif username!='' and password!="":
+        return "\033[91mNome de usuário já existe. Por favor, escolha outro.\033[0m"
+    elif username == password:
+        return "\033[91mUsuario e senhas iguais, tente novamente\033[0m"
+    elif username != '' and password != "":
         cursor.execute("INSERT INTO usuarios (username, password, saldo) VALUES (?, ?, ?)", (username, password, 0))
         conn.commit()
-        return "Cadastro realizado com sucesso!"
+        return "\033[92mCadastro realizado com sucesso!\033[0m"
     else:
-        print("usuario ou senha vazio")
+        return "\033[91mUsuário ou senha vazio\033[0m"
+
 
 def Login_usuario(username, password):
     cursor.execute("SELECT * FROM usuarios WHERE username = ? AND password = ?", (username, password))
@@ -51,72 +51,77 @@ def Login_usuario(username, password):
         return user
     else:
         return None
+
+
 def horario():
-    hora=datetime.now()
-    hora=hora.strftime("%d/%m/%Y %H:%M")
+    hora = datetime.now()
+    hora = hora.strftime("%d/%m/%Y %H:%M")
     return hora
+
+
 def opcoes_banco(username, saldo):
     while True:
-        opc1 = input(f'''
-{"="*30}
-DIGITE A OPERAÇÃO DESEJADA
-1) Consultar saldo
-2) Saque 
-3) Depositar
-4) Deletar conta
-5) Pesquisar conta
-0) Encerrar sessão: 
-{"="*30}\n''')
+        print("=" * 33)
+        print("\033[94mDIGITE A OPERAÇÃO DESEJADA\033[0m")
+        print("\033[94m1) Consultar saldo\033[0m")
+        print("\033[94m2) Saque\033[0m")
+        print("\033[94m3) Depositar\033[0m")
+        print("\033[94m4) Deletar conta\033[0m")
+        print("\033[94m5) Pesquisar conta\033[0m")
+        print("\033[94m0) Encerrar sessão\033[0m")
+        print("=" * 33)
+        opc1 = input()
         if opc1 == "1":
-            print( "Seu saldo é de R$ {}".format(saldo))
+            print("Seu saldo é de R$", saldo)
         elif opc1 == "2":
-            saque = float(input("Qual valor você deseja sacar? "))       
+            saque = float(input("Qual valor você deseja sacar? "))
             if saldo < saque:
-                print("\nValor insuficiente")
+                print("\033[91mValor insuficiente\033[0m")
             else:
                 saldo -= saque
                 cursor.execute("UPDATE usuarios SET saldo = ? WHERE username = ?", (saldo, username))
                 conn.commit()
-                relato_saque=f"Saque de R$ {saque:.2f} realizado com sucesso."
-                print(relato_saque)
-                estrato.append([username,relato_saque,horario()])
+                relato_saque = f"Saque de R$ {saque:.2f} realizado com sucesso."
+                print("\033[92m" + relato_saque + "\033[0m")
+                estrato.append([username, relato_saque, horario()])
         elif opc1 == "3":
-            deposito=float(input("Qual valor que deseja depositar? "))
+            deposito = float(input("Qual valor que deseja depositar? "))
             saldo += deposito
             cursor.execute("UPDATE usuarios SET saldo = ? WHERE username = ?", (saldo, username))
             conn.commit()
-            relato_deposito=f"Depósito de R$ {deposito} realizado."
-            print(relato_deposito)
-            estrato.append([username,relato_deposito,horario()])
+            relato_deposito = f"Depósito de R$ {deposito} realizado."
+            print("\033[92m" + relato_deposito + "\033[0m")
+            estrato.append([username, relato_deposito, horario()])
         elif opc1 == "4":
-            deletar_conta=input("Deseja deletar sua conta? (S/N)")
+            deletar_conta = input("Deseja deletar sua conta? (S/N)")
             if deletar_conta.lower() == "s":
                 cursor.execute("DELETE FROM usuarios WHERE username = ?", (username,))
                 conn.commit()
-                print("Conta deletada.")
+                print("\033[92mConta deletada.\033[0m")
                 return
-        elif opc1=='5':
-            pesquisa=input("digite nome do usuario ")
-            cursor.execute("SELECT username FROM usuarios WHERE username= ?",(pesquisa,))
+        elif opc1 == '5':
+            pesquisa = input("digite nome do usuário ")
+            cursor.execute("SELECT username FROM usuarios WHERE username= ?", (pesquisa,))
             pesquisa = cursor.fetchone()
             if pesquisa:
-                print(f"usuario {pesquisa[0]} encontrado")
+                print("\033[92mUsuário {} encontrado\033[0m".format(pesquisa[0]))
             else:
-                print("nao encontrado")
+                print("\033[91mNão encontrado\033[0m")
         elif opc1 == "0":
-            print("Encerrando sessão...")
+            print("\033[92mEncerrando sessão...\033[0m")
             return
         else:
-            print("Opção inválida.")
+            print("\033[91mOpção inválida.\033[0m")
+
 
 while True:
-    print("="*30)
-    opc = input(f'''
-    1. Cadastrar
-    2. Login
-    3. Sair
-    Escolha uma opção: ''')
-    print("="*30)
+    print("=" * 33)
+    print("\033[94m1. Cadastrar\033[0m")
+    print("\033[94m2. Login\033[0m")
+    print("\033[94m3. Sair\033[0m")
+    print("=" * 33)
+
+    opc = input("Escolha uma opção: ")
 
     if opc == "1":
         username = input("Digite um nome de usuário: ")
@@ -127,13 +132,14 @@ while True:
         password = pwinput.pwinput("Digite sua senha: ", mask="*")
         user = Login_usuario(username, password)
         if user:
-            print("Login bem-sucedido!")
+            print("\033[92mLogin bem-sucedido!\033[0m")
             opcoes_banco(username, user[2])  # Passa o username e o saldo do usuário
         else:
-            print("Nome de usuário ou senha incorretos. Tente novamente.")
+            print("\033[91mNome de usuário ou senha incorretos. Tente novamente.\033[0m")
     elif opc == "3":
-        print("Saindo...")
-        break   
+        print("\033[92mSaindo...\033[0m")
+        break
     else:
-        print("Opção inválida. Por favor, escolha novamente.")
+        print("\033[91mOpção inválida. Por favor, escolha novamente.\033[0m")
+
 extrato()
