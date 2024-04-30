bc56336d60fb046965c74c8b27d83a068a994f95
erickovisck <erickferrnandomsantos@gmail.com>
2024-04-30 19:02:10 -0300

    cabecalho

--- CRUD/CRUD.py
+++ CRUD/CRUD.py
@@ -1,3 +1,11 @@
+#UMC ADS - 1°D
+#Nomes: 
+#       Eduardo Vicente Ferreira das Neves: 11241104345
+#       Erick Fernando Martins Santos: 11241504537
+#       Rafael da Silva Castro: 11241104987
+#       Thais Perreira de Oliveira: 11241103553
+
+#Pré Projeto Crud em Python
 import os
 import sqlite3
 import pwinput
