fdb977d2164a0c9a4387e821749a95df46a8bbf8
erickovisck <erickferrnandomsantos@gmail.com>
2024-05-01 17:17:26 -0300

    arrumando

--- Exercicios_Diversos/tela/fibonacci.py
+++ Exercicios_Diversos/tela/fibonacci.py
@@ -3,11 +3,18 @@ quadrado=turtle.Turtle()
 circulo=turtle.Turtle()
 circulo.hideturtle()
 quadrado.hideturtle()
-circulo.speed(7)
-quadrado.speed(7)
+
+quadrado.speed(0)
 fibonnaci=15
 anterior, proximo=1,1
-
+for step in range(2):
+  
+    for _ in range (5):
+        quadrado.forward(proximo)
+        quadrado.left(90)
+    quadrado.forward(proximo)  
+    circulo.circle(proximo, 90) 
+   
 for step in range(fibonnaci):
     proximo+=anterior
     for _ in range (5):
