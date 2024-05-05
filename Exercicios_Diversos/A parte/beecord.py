d34649632411b24a80f9686cceb616c9b6322a88
erickovisck <erickferrnandomsantos@gmail.com>
2024-05-05 18:09:23 -0300

    beecrowd numero extenso

--- Exercicios_Diversos/A parte/beecord.py
+++ Exercicios_Diversos/A parte/beecord.py
@@ -60,16 +60,49 @@ import re
 #     soma2=soma
 # print(soma2)
 
-#nao entendi bulhufas desse
+#nao entendi bulhufas desse 
+#ENTENDI
+# ^ soma os valores binarios do numero, caso a casa binaria de ambos retornem True(1) reescreve como false(0)
+# 5(00000101) + 3(00000011) = 8(00000110)
 # while True:
 #     try:
 #         a, b = [int(x) for x in input().strip().split(' ')]
 #         print(a ^ b)
 #     except EOFError:
 #         break
-lista=[]
-num=int(input)
-for i in range(num):
-    lista.append(i)
-while len(lista)>1:
-    
+
+#xadrez
+
+# n=int(input(""))
+# xadrez=n*n
+# branco,preto=0,0
+# if 2<=n<=100:
+#     for i in range(0,xadrez):
+#         if i%2==0:
+#             branco+=1
+#         else:
+#             preto+=1
+#     print(f"{branco} casas brancas e {preto} casas pretas")
+
+#AOT
+
+# n, x = [int(x) for x in input().strip().split(' ')]
+# t = input("")
+# print(n,x)
+
+#numero extenso
+
+x=int(input(""))
+s=(int(str(x)[:1])-2)
+numeros1=["one ","two","three","four","five","six","seven","eight","nine","ten","eleven","Twelve","Thirteen","Fourteen","Fifteen","sixteen","seventeen","eighteen","nineteen"]
+numeros2=["twenty ","tirty ","fourty ","fifty ","sixty ","seventy ","eighty ","ninety "]
+if 1<=x<=100:
+    if x<=19:
+        palavra=numeros1[x-1]
+    elif str(x)[1:]=='0':
+        palavra=numeros2[s].strip()
+    elif x==100:
+        palavra="one hundred"
+    else:
+        palavra=numeros2[s]+numeros1[int(str(x)[1:])-1]
+print(palavra,len(palavra))
--- Exercicios_Diversos/Untitled-1.py
+++
@@ -1,5 +0,0 @@
-nome="erick"
-if nome=="erick" and nome:
-    print("Oi")
-if nome=="erick" or nome=="thais":
-    print("Ola")
