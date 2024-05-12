3add66bfff434e1440d3812c55cc6c634666bede
Thais Pereira de Oliveira <168770742+thataax@users.noreply.github.com>
2024-05-11 22:49:30 -0300

    novo CRUDtxt.py

--- CRUD/CRUDtxt.py
+++ CRUD/CRUDtxt.py
@@ -16,14 +16,14 @@ os.system('cls')
 
 # Conexão com o banco de dados SQLite
 conn = sqlite3.connect('banco.db')
-cursor = conn.cursor()
+cursor = conn.cursor() #tem q tirar isso aqui, né? ou nn
 
 # Criando a tabela de usuários se não existir
 cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
                     username TEXT PRIMARY KEY,
                     password TEXT,
                     saldo INTEGER
-                )''')
+                )''')  #tem q tirar isso aqui, né? ou nn
 
 conn.commit()
 
@@ -37,3 +37,17 @@ def extrato():
     conn.close()
 
 #txt
+def Cadastro_usuario(username, password):
+    arquivo_cadastrotxt = open ("crud\cadastrotxt", "w")
+    for linha in arquivo_cadastrotxt:
+      arquivo_cadastrotxt.readline(" ")
+      if username in linha:
+       return "\033[91mNome de usuário já existe. Por favor, escolha outro.\033[0m"
+      elif username == password:
+        return "\033[91mUsuario e senhas iguais, tente novamente\033[0m"
+      elif username != '' and password != "":
+        cursor.execute(".White usuarios (username, password, saldo) VALUES (?, ?, ?)", (username, password, 0))
+        conn.commit()
+        return "\033[92mCadastro realizado com sucesso!\033[0m"
+      else:
+        return "\033[91mUsuário ou senha vazio\033[0m"
